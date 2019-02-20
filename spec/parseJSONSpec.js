691c1e3de025906b2dad441e3f0e52d68568849e
jfeng3124 <jiafeng3124@gmail.com>
2019-02-19 21:44:46 -0800

    fixed test

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -6,9 +6,7 @@ describe('parseJSON', function() {
       var result = parseJSON(test);
       var expected = JSON.parse(test);
       var equality = _.isEqual(result, expected); // why can't we use `===` here?
-      // Replace this line with an `expect` statement that tests
-      // the behavior described by the `it` string
-      throw new Error('Test is missing.')
+      expect(equality).to.equal(true);
     });
   });
 
@@ -16,7 +14,7 @@ describe('parseJSON', function() {
     unparseableStrings.forEach(function(test) {
       var fn = function() {
         parseJSON(test);
-      };
+      };       
       // if you'd prefer, you can write your version of parseJSON 
       // so that it passes this test instead of the one on line 21. 
       // expect(parseJSON(test)).to.equal(undefined);
